d2a4a14333157136aa7abb06174bf2dbe593f10c
arthur mario <artmario@gmail.com>
2020-02-20 10:52:25 -0400

    alteração view
    usando bootstrap

--- resources/views/series/index.php
+++ resources/views/series/index.php
@@ -5,13 +5,22 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <title>Controle de Séries</title>
 </head>
 <body>
-    <ul>
+<div class="container">
+    <div class="jumbotron">
+        <h1>
+            series
+        </h1>
+    </div>
+    <a href="" class="btn btn-dark mb-2"> teste</a>
+    <ul class="list-group">
         <?php foreach($series as $serie): ?>
-        <li><?= $serie; ?></li>
+        <li class="list-group-item"><?= $serie; ?></li>
         <?php endforeach; ?>
     </ul>
+</div>
 </body>
 </html>
