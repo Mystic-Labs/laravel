fcfd5dd1d6affcd0b7c526557ed413f1ff01ac16
Murilo <murilo13campos@gmail.com>
2020-02-26 10:23:38 -0400

    Atividade 3

--- app/Http/Controllers/TemporadasController.php
+++ app/Http/Controllers/TemporadasController.php
@@ -2,14 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Serie;
 use Illuminate\Http\Request;
 
 class TemporadasController extends Controller
 {
     public function index(int $serieId)
     {
-        $temporadas = Serie::find($serieId)->temporadas;
+        $serie = Serie::find($serieId);
+        $temporadas = $serie->temporadas;
 
-        return view('temporadas.index', compact('temporadas'));
+        return view('temporadas.index', compact('serie','temporadas'));
     }
 }
