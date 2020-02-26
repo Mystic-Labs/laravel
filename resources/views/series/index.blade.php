cf7af7bef8d2df953a4cc64d51bb1d4c0bbab806
arthur mario <artmario@gmail.com>
2020-02-26 11:51:07 -0400

    interface html edição

--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -12,7 +12,17 @@
     <ul class="list-group">
         @foreach($series as $serie)
             <li class="list-group-item d-flex justify-content-between align-items-center">
-                {{ $serie->nome  }}
+                <span id="nome-serie-{{ $serie->nome  }}"> {{ $serie->nome  }}</span>
+                <div class="input-group w-50" hidden id="input-nome-serie-{{$serie->nome}}">
+                    <input type="text" class="form-control" value="{{ $serie->nome  }}">
+                    <div class="input-group-append">
+                        <button class="btn btn-primary" onclick="editarSerie({{ $serie->nome  }})">
+                        <i class="fas fa-check"></i>
+                        </button>
+                        @csrf
+                    </div>
+                </div>
+
                 <span class="d-flex">
                         <a href="/series/{{ $serie->id  }}/temporadas" class="btn btn-info btn-sm mr-1">
                             <i class="fas fa-external-link-alt"></i>
