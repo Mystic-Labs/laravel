c98e6f1bbaab183bdcaed09afe9bd422997be5e1
Murilo <murilo13campos@gmail.com>
2020-02-26 13:04:13 -0400

    finalizando ediçao de nome da serie

--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -20,7 +20,7 @@
                 <div class="input-group w-50" hidden id="input-nome-serie-{{$serie->id}}">
                     <input type="text" class="form-control" value="{{ $serie->nome  }}">
                     <div class="input-group-append">
-                        <button class="btn btn-primary" onclick="editarSerie({{ $serie->nome  }})">
+                        <button class="btn btn-primary" onclick="editarSerie({{ $serie->id  }})">
                             <i class="fas fa-check"></i>
                         </button>
                         @csrf
@@ -54,12 +54,29 @@
                 nomeSerieEl.removeAttribute('hidden');
                 inputSerieEl.hidden = true;
             }
-            
+
             else {
                 inputSerieEl.removeAttribute('hidden');
                 nomeSerieEl.hidden = true;
             }
 
+        }
+
+        function editarSerie(serieId){
+            let formData = new FormData();
+            const nome = document.querySelector(`#input-nome-serie-${serieId} > input`).value;
+            const token = document.querySelector('input[name="_token"]').value;
+            formData.append('nome', nome);
+            formData.append('_token', token);
+
+            const url = `/series/${serieId}/editaNome`;
+            fetch(url, {
+                body: formData,
+                method: 'POST'
+            }).then(() => {
+                toggleInput(serieId);
+                document.getElementById(`nome-serie-${serieId}`).textContent = nome;
+            });
         }
     </script>
 @endsection
