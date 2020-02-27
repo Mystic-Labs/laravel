5a96f8981c601cc1ea77df8f85bd67952927667b
Murilo <murilo13campos@gmail.com>
2020-02-27 11:04:10 -0400

    form registrar

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -16,6 +16,11 @@ use Illuminate\Support\Facades\Auth;
 class SeriesController extends Controller
 {
 
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index(Request $request)
     {
         $series = Serie::query()
