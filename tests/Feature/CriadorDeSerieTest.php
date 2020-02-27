80c4f3e41692374971ccb3d54e77b06426d60d7d
Murilo <murilo13campos@gmail.com>
2020-02-27 14:56:29 -0400

    mais testes

--- tests/Feature/CriadorDeSerieTest.php
+++ tests/Feature/CriadorDeSerieTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Serie;
 use App\Services\CriadorDeSerie;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -9,11 +10,8 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class CriadorDeSerieTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
+    use RefreshDatabase;
+
     public function testCriarSerie()
     {
         $criadorDeSerie = new CriadorDeSerie();
@@ -22,5 +20,7 @@ class CriadorDeSerieTest extends TestCase
 
         $this->assertInstanceOf(Serie::class, $serieCriada);
         $this->assertDatabaseHas('series', ['nome' => $nomeSerie]);
+        $this->assertDatabaseHas('temporadas', ['serie_id' => $serieCriada->id, 'numero' => 1]);
+        $this->assertDatabaseHas('episodios', ['numero' => 1]);
     }
 }
