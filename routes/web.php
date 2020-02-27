aaed64269e8bb79e105de18451b294120ce0a36d
Murilo <murilo13campos@gmail.com>
2020-02-27 10:21:17 -0400

    contando episodios assistidos e mostrando pro usuario

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,5 @@ Route::post('/series/{id}/editaNome', 'SeriesController@editaNome');
 Route::get('/series/{serieId}/temporadas', 'TemporadasController@index');
 
 Route::get('/temporadas/{temporada}/episodios', 'EpisodiosController@index');
+
+Route::post('/temporadas/{temporada}/episodios/assistir', 'EpisodiosController@assistir');
